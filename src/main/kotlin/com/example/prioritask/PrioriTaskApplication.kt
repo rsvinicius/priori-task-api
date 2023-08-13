6b6b28742187bb6bae1b992dd00ad3dbac26b6dc
Vinicius R <vrodrigues.code@gmail.com>
2023-08-13 16:51:01 -0300

    Spring initializr

---
+++ src/main/kotlin/com/example/prioritask/PrioriTaskApplication.kt
@@ -0,0 +1,11 @@
+package com.example.prioritask
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PrioriTaskApplication
+
+fun main(args: Array<String>) {
+	runApplication<PrioriTaskApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/prioritask/PrioriTaskApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.prioritask
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PrioriTaskApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
